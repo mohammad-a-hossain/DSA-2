285533cf6bf88573e0b8c210ac015ec4dd972fee
mohammad-a-hossain <adam.eve007@gmail.com>
2023-06-08 11:35:55 +0600

    anagram paractise

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-function validAnagram(arr1,arr2){
 
-    // add whatever parameters you deem necessary - good luck!
-    if(arr1.length !== arr2.length) return false
-    arr1= arr1.toLowerCase()
-    arr2 = arr2.toLowerCase()
- 
-    const charObj = {};
-    for(let i=0 ;i<arr1.length;i++){
-      charObj[arr1[i]]=  charObj[arr1[i]]+1 || 1;
-        }
-    for (let i = 0; i < arr2.length; i++) {
-          if(!charObj[arr2[i]]){
-             return false
-          }else {
-          charObj[arr1[i]]=charObj[arr1[i]]--
-          }
-         }
-    return true
+// valid anagram 
+
+// if letters are compared to other array lik e abc = a:1 b:2 c:3 cba= abc equal a;1 b:1 :c:1
+// find if two array of string are same or not 
+// problem
+
+function validAnagram(arr1, arr2){
+  if(arr1.length !== arr2.length) return false
+
+
+  let getObj={}
+
+  for(let i=0; i< arr1.length; i++){
+    let letter = arr1[i]
+   getObj[letter] ? getObj[letter] += 1: getObj[letter] = 1 
   }
-  
+ 
+  for(let i=0; i < arr2.length; i++){
+    let letter= arr2[i]
+    if(!getObj[letter]){
+      return false
+    }else{
+      getObj[letter] -= 1
+    }
+  }
+  return true
+}
 
-   console.log(validAnagram('texttwisttime', 'timetwisttext') );
+
+const  result = 
+validAnagram('bangladesh','bangladesh')
+console.log(result)
+const  result1 = 
+validAnagram('abc','ac')
+console.log(result1)
+
+
+
+
